63fe5deffbfb546e0dc079b4391e688531b55419
Bojan desktopPc <bojanbdjp@gmail.com>
2020-10-09 20:38:36 +0200

    changes api

--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: 'https://natco-app.herokuapp.com'
-    baseURL: 'http://2e20aebedcba.ngrok.io'
+     baseURL: 'https://natco-app.herokuapp.com'
 })
 
-// export const URL = 'https://natco-app.herokuapp.com';
-export const URL = 'http://2e20aebedcba.ngrok.io';
+ export const URL = 'https://natco-app.herokuapp.com';
 
 
 export const IMGBB_API_KEY = 'a3a9f45cf424d862d32867dc1e9fae32';
